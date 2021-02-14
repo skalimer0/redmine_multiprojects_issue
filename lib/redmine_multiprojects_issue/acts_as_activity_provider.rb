0d245545896f1e3ae102e9e9115b2fc7b229bad4
Brice BEAUMESNIL <brice.beaumesnil@gmail.com>
2021-02-14 09:22:35 +0100

    redmine master compliant

--- lib/redmine_multiprojects_issue/acts_as_activity_provider.rb
+++ lib/redmine_multiprojects_issue/acts_as_activity_provider.rb
@@ -1,15 +1,68 @@
+# frozen_string_literal: true
+
+# Redmine - project management software
+# Copyright (C) 2006-2020  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 module Redmine
   module Acts
     module ActivityProvider
+      def self.included(base)
+        base.extend ClassMethods
+      end
+
+      module ClassMethods
+        def acts_as_activity_provider(options = {})
+          unless self.included_modules.include?(Redmine::Acts::ActivityProvider::InstanceMethods)
+            cattr_accessor :activity_provider_options
+            send :include, Redmine::Acts::ActivityProvider::InstanceMethods
+          end
+
+          options.assert_valid_keys(:type, :permission, :timestamp, :author_key, :scope)
+          self.activity_provider_options ||= {}
+
+          # One model can provide different event types
+          # We store these options in activity_provider_options hash
+          event_type = options.delete(:type) || self.name.underscore.pluralize
+
+          options[:timestamp] ||= "#{table_name}.created_on"
+          options[:author_key] = "#{table_name}.#{options[:author_key]}" if options[:author_key].is_a?(Symbol)
+          self.activity_provider_options[event_type] = options
+        end
+      end
+
       module InstanceMethods
+        def self.included(base)
+          base.extend ClassMethods
+        end
+
         module ClassMethods
           # Returns events of type event_type visible by user that occurred between from and to
           def find_events(event_type, user, from, to, options)
-
             provider_options = activity_provider_options[event_type]
             raise "#{self.name} can not provide #{event_type} events." if provider_options.nil?
 
-            scope = (provider_options[:scope] || self)
+            scope = provider_options[:scope]
+            if !scope
+              scope = self
+            elsif scope.respond_to?(:call)
+              scope = scope.call
+            else
+              ActiveSupport::Deprecation.warn "acts_as_activity_provider with implicit :scope option is deprecated. Please pass a scope on the #{self.name} as a proc."
+            end
 
             ## START PATCH
             ## START PATCH
@@ -53,5 +106,3 @@ module Redmine
     end
   end
 end
-
-ActiveRecord::Base.send(:include, Redmine::Acts::ActivityProvider)
